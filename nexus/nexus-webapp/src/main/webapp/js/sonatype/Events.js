d843990110e821a3865282691ad2099c5c22c6e7
Jason Dillon <jason@planet57.com>
2012-05-08 22:43:28 -0700

    notes

--- nexus/nexus-webapp/src/main/webapp/js/sonatype/Events.js
+++ nexus/nexus-webapp/src/main/webapp/js/sonatype/Events.js
@@ -10,6 +10,9 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
+
+// FIXME: Why is this called Sonatype.utils.Observable?
+
 Sonatype.utils.Observable = function() {
   this.addEvents({
         /*
